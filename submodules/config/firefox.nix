e515f243088984f78f5e5b260ea0d9a2048866ab
vackneex <vackneex@proton.me>
2026-03-28 13:36:25 +0100

    firefox config update

--- submodules/config/firefox.nix
+++ submodules/config/firefox.nix
@@ -24,6 +24,13 @@
           Locked = true;
         };
         Homepage.StartPage = "previous-session";
+        SearchEngines.Default = "DuckDuckGo";
+
+        ExtensionsSettings = {
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = { # Bitwarden
+            global_autofillSettingsLocal_inlineMenuVisibility = ''{"__json__": true, "value": "2"}'';
+          };
+        };
       };
 
       nativeMessagingHosts = [ pkgs.kdePackages.plasma-browser-integration ];
