d733db6feb417df1c920347e96a4fda62c2b44bf
vackneex <vackneex@proton.me>
2026-03-28 21:22:37 +0100

    kde panels cfg

--- submodules/config/plasma.nix
+++ submodules/config/plasma.nix
@@ -27,9 +27,63 @@
         };
       };
 
-      # desktop = {
-      #   widgets = [];
-      # };
+      panels = [
+        {
+          alignment = "center";
+          floating = true;
+          hiding = "autohide";
+          lengthMode = "fit";
+          location = "bottom";
+
+          widgets = [
+            "org.kde.plasma.kickoff"
+            "org.kde.plasma.icontasks"
+          ];
+        }
+        {
+          alignment = "center";
+          lengthMode = "fill";
+          location = "top";
+          height = 26;
+
+          widgets = [
+            {
+              applicationTitleBar = {
+                layout = {
+                  elements = [
+                    "windowIcon"
+                    "windowTitle"
+                  ];
+
+                  widgetMargins = 0;
+                };
+
+                windowTitle.undefinedWindowTitle = "No Focus";
+              };
+            }
+            "org.kde.plasma.panelspacer"
+            {
+              digitalClock = {
+                date.enable = false;
+              };
+            }
+            "org.kde.plasma.panelspacer"
+            "org.kde.plasma.systemmonitor.diskusage"
+            "org.kde.plasma.systemmonitor.cpu"
+            "org.kde.plasma.systemmonitor.memory"
+            {
+              systemTray = {
+                items.hidden = [
+                  "org.kde.plasma.clipboard"
+                  "org.kde.plasma.brightness"
+                  "org.kde.plasma.mediacontroller"
+                ];
+              };
+            }
+            "org.kde.plasma.showdesktop"
+          ];
+        }
+      ];
     };
   };
 }
