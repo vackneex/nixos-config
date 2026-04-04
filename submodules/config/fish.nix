8c1798890ee44cd8258b1c4617d03e315db05ecc
vackneex <vackneex@proton.me>
2026-04-04 20:04:14 +0200

    huh?

--- submodules/config/fish.nix
+++ submodules/config/fish.nix
@@ -7,6 +7,7 @@
         set -g fish_greeting ""
         direnv hook fish | source
       '';
+      
       shellAliases = {
         clean-user = "nix-collect-garbage -d";
         clean-system = "sudo nix-env --delete-generations +5 -p /nix/var/nix/profiles/system && sudo nix-collect-garbage -d";
