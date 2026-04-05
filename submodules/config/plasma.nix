dd7e23a9e0b4464dd6467f2fcf415695a7da47ea
vackneex <vackneex@proton.me>
2026-04-05 11:40:18 +0200

    xd

--- submodules/config/plasma.nix
+++ submodules/config/plasma.nix
@@ -5,19 +5,18 @@
       enable = true;
       workspace.lookAndFeel = "org.kde.breezedark.desktop";
 
-      input = {
-        keyboard = {
-          layouts = [
-            { layout = "ru"; }
-            { layout = "ua"; }
-            { layout = "pl"; }
-          ];
-          numlockOnStartup = "on";
-          options = [
-            "grp:alt_shift_toggle"
-            "caps:backspace"
-          ];
-        };
+      input.keyboard = {
+        layouts = [
+          { layout = "ru"; }
+          { layout = "ua"; }
+          { layout = "pl"; }
+        ];
+        
+        numlockOnStartup = "on";
+        options = [
+          "grp:alt_shift_toggle"
+          "caps:backspace"
+        ];
       };
 
       panels = [
