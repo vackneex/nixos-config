48d62f0a202599de2afc3a1c1453e296c711d28c
vackneex <vackneex@proton.me>
2026-04-05 21:12:16 +0200

    updated hostname

--- submodules/hosts/gl552vw/host.nix
+++ submodules/hosts/gl552vw/host.nix
@@ -1,17 +1,17 @@
 { inputs, self, lib, ... }: {
 
-  flake.nixosConfigurations.asus-gl552vw = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.gl552vw = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
       self.nixosModules.shared-host-config
-      self.nixosModules.asus-gl552vw-config-hardware
+      self.nixosModules.gl552vw-config-hardware
       self.nixosModules.home-manager
 
       inputs.disko.nixosModules.disko
       self.nixosModules.shared-disko-config
       { 
         host-system.os-disk = "/dev/disk/by-id/ata-SAMSUNG_MZNLF128HCHP-00004_S2DNNXAH219864";
-        host-system.hostname = "asus-gl552vw";
+        host-system.hostname = "gl552vw";
       }
     ];
   };
